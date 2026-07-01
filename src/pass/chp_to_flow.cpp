7ec4fa45fcd567556bf82d22e42235c69a7c8dc9
Ned Bingham <edward.bingham@broccolimicro.io>
2026-06-30 21:38:15 -0400

    separating new sub variant creation from updating the parent

--- src/pass/chp_to_flow.cpp
+++ src/pass/chp_to_flow.cpp
@@ -3,6 +3,8 @@
 #include <chp/synthesize.h>
 #include <gc/guarded_command.h>
 
+#include <interpret_gc/export.h>
+
 bool flatten(const Build &builder, weaver::Program &prgm, weaver::TermId id) {
 	if (not id.hasVar() or prgm.varAt(id).meta.dialect != "func") {
 		return false;
@@ -29,7 +31,7 @@ bool decompose(Build &builder, weaver::Program &prgm, weaver::TermId id) {
 		return false;
 	}
 
-	chp::graph &g = prgm.varAt(id).as<chp::graph>();
+	chp::graph g = prgm.varAt(id).as<chp::graph>();
 
 	vector<chp::graph> sub = g.decompose();
 	if (sub.size() <= 1) {
@@ -38,25 +40,27 @@ bool decompose(Build &builder, weaver::Program &prgm, weaver::TermId id) {
 	}
 
 	string dialect = prgm.varAt(id).meta.dialect;
-	id.var = prgm.termAt(id).createVariant(weaver::Variant("struct", std::any(), id.var));
+	id.var = prgm.termAt(id).createVariant(weaver::Variant("struct", gc::GuardedCommands(), id.var));
+	gc::GuardedCommands &rules = prgm.varAt(id).as<gc::GuardedCommands>();
+	for (size_t pid = 0; pid < sub.size(); pid++) {
+		arithmetic::Action action(arithmetic::call(sub[pid].name, {}));
+		arithmetic::Choice choice({{action}});
+		cout << "LOOK GC ACTION '" << sub[pid].name << "' action=" << action << " choice=" << choice << endl;
+		rules.rules.push_back(gc::GuardedCommand(choice));
+		cout << "export=" << gc::export_rule(rules.rules.back(), rules).to_string() << endl;
+	}
+	for (size_t i = 0; i < g.vars.size(); i++) {
+		rules.vars.push_back(gc::Variable(g.vars[i].name, g.vars[i].region));
+		rules.vars.back().remote = g.vars[i].remote;
+	}
 	builder.todo.push_back(id);
 
-	gc::GuardedCommands rules;
 	for (size_t pid = 0; pid < sub.size(); pid++) {
-		vector<weaver::Instance> args = prgm.termAt(id).decl.args;
-		for (int j = (int)args.size()-1; j >= 0; j--) {
-			if (g.netIndex(args[j].name) < 0) {
-				args.erase(args.begin()+j);
-			}
-		}
-
+		vector<weaver::Instance> args;
 		weaver::TermId subId = prgm.createTerm(id.mod, weaver::Term(sub[pid].name, args));
 		subId.var = prgm.termAt(subId).createVariant(weaver::Variant(dialect, sub[pid]));
 		builder.todo.push_back(subId);
-
-		rules.rules.push_back(gc::GuardedCommand(arithmetic::Choice({arithmetic::Parallel({arithmetic::Action(arithmetic::call(prgm.termAt(subId).decl.name, {}))})})));
 	}
-	prgm.varAt(id).set(rules);
 
 	return true;
 }
